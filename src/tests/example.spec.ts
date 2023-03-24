72e6e6424273a237a94846d05006919417d06e20
Dylan Llewellyn <46717769+herefishyfish@users.noreply.github.com>
2023-03-24 18:35:59 +0800

    ns test init

---
+++ src/tests/example.spec.ts
@@ -0,0 +1,9 @@
+import { Application } from "@nativescript/core";
+
+// A sample Jasmine test
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const webpack = require("@nativescript/webpack");
+const { merge } = require("webpack-merge");
 
 module.exports = (env) => {
-	webpack.init(env);
+  webpack.init(env);
 
-	// Learn how to customize:
-	// https://docs.nativescript.org/webpack
+  webpack.chainWebpack((config) => {
+    if (env.unitTesting == true) {
+      config.plugin("DefinePlugin").tap((args) => {
+        args[0] = merge(args[0], {
+          __TEST_RUNNER_STAY_OPEN__: true,
+        });
+        return args;
+      });
+    }
+  });
 
-	return webpack.resolveConfig();
+  return webpack.resolveConfig();
 };
